45e029472426700b75c1a6f1a390a9b283570c04
Kate <katehughes2424@gmail.com>
2020-11-09 11:36:02 +0000

    Removed unwanted classes

--- src/main/java/com/pluralsight/client/PostClient.java
+++ src/main/java/com/pluralsight/client/PostClient.java
@@ -1,15 +1,7 @@
 package com.pluralsight.client;
 
-import com.pluralsight.model.Activity;
-import com.pluralsight.model.Post;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
 
 public class PostClient {
 
--- src/main/java/com/pluralsight/model/ActivitySearchType.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.model;
-
-public enum ActivitySearchType {
-
-    SEARCH_BY_DURATION_RANGE, SEARCH_BY_DESCRIPTION;
-
-}
